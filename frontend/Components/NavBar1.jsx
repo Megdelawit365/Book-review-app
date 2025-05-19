7c4de6e73885a642ba4785757a3f4f6a22feaabf
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-05-19 11:00:49 +0300

    Update NavBar1.jsx

--- frontend/Components/NavBar1.jsx
+++ frontend/Components/NavBar1.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
-import { LiaSwatchbookSolid } from "react-icons/lia";
+import { MdLibraryBooks } from "react-icons/md";
 import { Link } from 'react-router-dom';
+import { FaBookBookmark } from "react-icons/fa6";
 
 
 const NavBar1 = () => {
   return (
-    <nav className='flex-row'>
-      <Link to='/' className='flex-row big-font'>
-        <LiaSwatchbookSolid />
+    <nav className='flex-row nav2'>
+      <Link to='/' className='flex-row big-font ' style={{width:"100%"}}>
+        <FaBookBookmark />
+
         <p>BOOKLIFY</p>
+        <div className='flex-row align-right hide'>
+          <Link to='/explore-books'>Explore Books</Link>
+          <Link to='/login'><button >Login</button></Link>
+                    <Link to='/signup'><button className='button2' >Signup</button></Link>
+
+          
+          
+        </div>
+
       </Link>
     </nav>
   )
