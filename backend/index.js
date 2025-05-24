58cb1095200b61b8a2866b5e54b7bede0385cafb
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-05-24 14:53:29 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: ['http://localhost:5173','https://book-review-app-fawn.vercel.app/'],
+    origin: ['http://localhost:5173','https://book-review-app-fawn.vercel.app'],
     credentials: true
 }));
 app.use(cookieParser());
