ffb1082a160fdf972245f4b613e8e4c2feadbeba
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-07-12 20:52:51 +0300

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,7 @@ const Home = () => {
                 <section className='hero-section'>
                     <div className='hero-icons-container'>
                         <FiMail /><FaGithub /><FaTelegramPlane /><FaLinkedin /><FaPhone />
+                        <p style={{color:"red"}}>Still in Progress</p>
                     </div>
                     <h1>Discover <span>Books</span> . Read & Write Reviews. Save Your Favorites.</h1>
                     <p>A Place For Book Lovers To Explore and Share Book Reviews. Join And Find Your Next Great Read!</p>
