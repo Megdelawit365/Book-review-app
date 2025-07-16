9ec68fe55eb150405a608b8902eb8dcb120e32cb
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-07-16 20:43:56 +0300

    Update bookRoutes.js

--- backend/Routes/bookRoutes.js
+++ backend/Routes/bookRoutes.js
@@ -3,97 +3,28 @@ const Book = require("../models/Book-model");
 const mongoose = require("mongoose");
 const router = express.Router();
 
-router.get("/nyt", async (req, res) => {
+
+
+
+router.get("/api/books", async (req, res) => {
   try {
-    const fictionHardcover = await fetch(
-      `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.NYT_API}`
-    );
-    const combinedFiction = await fetch(
-      `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-fiction.json?api-key=${process.env.NYT_API}`
-    );
-    // const tradeFiction = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/paperback-trade-fiction.json?api-key=${process.env.NYT_API}`
-    // );
-    // const massFiction = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/mass-market-paperback.json?api-key=${process.env.NYT_API}`
-    // );
-    // const yaHardcover = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/young-adult-hardcover.json?api-key=${process.env.NYT_API}`
-    // );
-    // const yaPaperback = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/young-adult-paperback.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const middleGradeHardcover = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/middle-grade-hardcover.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const middleGradePaperback = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/middle-grade-paperback.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const childrensSeries = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/series-books.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const graphicManga = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/graphic-books-and-manga.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const combinedNonfiction = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/combined-print-and-e-book-nonfiction.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const nonfictionHardcover = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-nonfiction.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const nonfictionPaperback = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/paperback-nonfiction.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const adviceMisc = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/advice-how-to-and-miscellaneous.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const businessBooks = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/business-books.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const audioFiction = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/audio-fiction.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const audioNonfiction = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/audio-nonfiction.json?api-key=${process.env.NYT_API}`
-    // );
-
-    // const pictureBooks = await fetch(
-    //   `https://api.nytimes.com/svc/books/v3/lists/current/picture-books.json?api-key=${process.env.NYT_API}`
-    // );
-
-    const data = {
-      fictionHardcover: await fictionHardcover.json(),
-      combinedFiction: await combinedFiction.json(),
-      // tradeFiction: await tradeFiction.json(),
-      // massFiction: await massFiction.json(),
-      // yaHardcover: await yaHardcover.json(),
-      // yaPaperback: await yaPaperback.json(),
-      // middleGradeHardcover: await middleGradeHardcover.json(),
-      // middleGradePaperback: await middleGradePaperback.json(),
-      // childrensSeries: await childrensSeries.json(),
-      // graphicManga: await graphicManga.json(),
-      // nonfictionHardcover: await nonfictionHardcover.json(),
-      // adviceMisc: await adviceMisc.json(),
-    };
-    console.log("Fetched data:", data);
-    res.json(data);
+    const books = await Book.find();
+    res.json(books);
   } catch (err) {
-    console.error("Failed to fetch books:", err);
+    res.status(500).json({ error: "Failed to get books" });
   }
 });
 
-router.get("/searchGoogleBooks", async (req, res) => {
+router.delete("/api/books", async (req, res) => {
+  try {
+    await Book.deleteMany({});
+    res.json({ message: "All books deleted successfully." });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to delete books" });
+  }
+});
+
+router.get("/api/searchGoogleBooks", async (req, res) => {
   const query = req.query.q;
   try {
     const result = await fetch(
@@ -106,7 +37,7 @@ router.get("/searchGoogleBooks", async (req, res) => {
   }
 });
 
-router.get("/searchGoogleBooks/isbn", async (req, res) => {
+router.get("/api/searchGoogleBooks/isbn", async (req, res) => {
   const query = req.query.q;
   try {
     const result = await fetch(
@@ -119,7 +50,7 @@ router.get("/searchGoogleBooks/isbn", async (req, res) => {
   }
 });
 
-router.get("/searchGoogleBooks/author", async (req, res) => {
+router.get("/api/searchGoogleBooks/author", async (req, res) => {
   const query = req.query.q;
   try {
     const result = await fetch(
