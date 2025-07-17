d7f22fea2885dcaa62da549bd6c3161051a21dd9
Megdelawit365 <megdelawitaynalem365@gmail.com>
2025-07-17 11:47:03 +0300

    Update NavBar.jsx

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import '../Styles/NavBar.css'
-import { NavLink, Link } from 'react-router-dom'
-import { FaSearch } from 'react-icons/fa';
+import { NavLink, Link, useLocation } from 'react-router-dom'
+import { FaHome, FaSearch } from 'react-icons/fa';
+import { FiHome, FiCompass, FiUpload, FiSearch, FiX } from 'react-icons/fi';
+import { HiMenu } from 'react-icons/hi';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios'
@@ -9,9 +11,11 @@ import axios from 'axios'
 const apiURL = import.meta.env.VITE_API_URL
 
 const NavBar = () => {
+    const location = useLocation();
     const [searchQuery, setSearchQuery] = useState("")
     const [searchResult, setSearchResult] = useState([])
     const [searchLoading, setSearchLoading] = useState(true)
+    const [openSearch, setOpenSearch] = useState(false)
 
     useEffect(() => {
 
@@ -32,21 +36,81 @@ const NavBar = () => {
                 <NavLink to='/explore' className={({ isActive }) => isActive ? "active-links" : ""}>Explore</NavLink>
                 <NavLink to='/submit' className={({ isActive }) => isActive ? "active-links" : ""}>Submit</NavLink>
             </div>
-            <div className='small-screen-hide search-container'>
-                <form action="">
-                    <input onChange={(e) => { setSearchQuery(e.target.value) }} type="text" placeholder='Search for any book' />
-                    <FaSearch className='icon' />
-                </form>
-                <button><Link>Sign up</Link></button>
+            <button className='small-screen-show signup-btn-small'><Link to='/signup'>Sign up</Link></button>
+
+            <div className={`small-screen-search ${openSearch ? "open" : "close"}`}>
+                <div className='search-top'>
+                    <form action="">
+                        <input onChange={(e) => { setSearchQuery(e.target.value) }} type="text" placeholder='Search for any book' />
+                        <FaSearch className='icon' />
+                    </form>
+                    <FiX className='close-icon' onClick={() => setOpenSearch(false)} />
+                </div>
+
                 {
                     searchQuery != "" && searchResult ?
 
                         <div className='search-result'>
                             {searchResult.map((result, index) => {
-                                return <div key={index}>
+                                return <Link className='result-container' key={index} to={`/details/${result?.volumeInfo?.industryIdentifiers?.[1]?.identifier}`} state={{ isbn: result?.volumeInfo?.industryIdentifiers?.[1]?.identifier }}>
+                                    <img src={result?.volumeInfo?.imageLinks?.thumbnail || 'https://placehold.co/200x300?text=No+Cover'} alt="" />
+                                    <p  >{result.volumeInfo.title}</p>
+                                </Link>
+                            })}
+                            <Link>See All Results</Link>
+                        </div>
+                        : searchQuery != "" && !searchResult ?
+                            <div className='search-result'>
+                                <Link>No results found :(</Link>
+                            </div>
+                            : <></>
+                }
+            </div>
+
+
+
+
+            {
+                location.pathname != '/' &&
+                <div className='sliding-nav small-screen-show'>
+                    <div>
+                        <FiHome className='icon' />
+                        <NavLink to='/' className={({ isActive }) => isActive ? "active-sliding-links" : ""}>Home</NavLink>
+                    </div>
+                    <div>
+                        <FiCompass className='icon' />
+                        <NavLink to='/explore' className={({ isActive }) => isActive ? "active-sliding-links" : ""}>Explore</NavLink>
+
+                    </div>
+                    <div>
+                        <FiUpload className='icon' />
+                        <NavLink to='/submit' className={({ isActive }) => isActive ? "active-sliding-links" : ""}>Submit</NavLink>
+
+                    </div>
+                    <div onClick={() => setOpenSearch(true)}>
+                        <FiSearch className='icon' />
+                        <NavLink>Search</NavLink>
+
+                    </div>
+
+                </div>
+            }
+
+            <div className='small-screen-hide search-container'>
+                <form action="">
+                    <input onChange={(e) => { setSearchQuery(e.target.value) }} type="text" placeholder='Search for any book' />
+                    <FaSearch className='icon' />
+                </form>
+                <button><Link to='/signup'>Sign up</Link></button>
+                {
+                    searchQuery != "" && searchResult ?
+
+                        <div className='search-result'>
+                            {searchResult.map((result, index) => {
+                                return <Link className='result-container' key={index} to={`/details/${result?.volumeInfo?.industryIdentifiers?.[1]?.identifier}`} state={{ isbn: result?.volumeInfo?.industryIdentifiers?.[1]?.identifier }}>
                                     <img src={result?.volumeInfo?.imageLinks?.thumbnail} alt="" />
                                     <p  >{result.volumeInfo.title}</p>
-                                </div>
+                                </Link>
                             })}
                             <Link>See All Results</Link>
                         </div>
